4f97f2f9113b813736a0b06f52bd498486058102
vagrant <vagrant@vagrant.vm>
2016-05-16 01:31:09 +0000

    .

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
+mkdir -p ~/.vim/bundle
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim
-ln -sf ~/dotfiles/ftdetect ~/.vim
-ln -sf ~/dotfiles/indent ~/.vim
+ln -sf ~/dotfiles/colors ~/.vim/.
+ln -sf ~/dotfiles/ftdetect ~/.vim/.
+ln -sf ~/dotfiles/indent ~/.vim/.
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
-ln -sf ~/dotfiles/Brewfile ~/Brewfile
-mkdir -p ~/.vim/bundle
+#ln -sf ~/dotfiles/Brewfile ~/Brewfile
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 echo "run: curl -L git.io/nodebrew | perl - setup"
