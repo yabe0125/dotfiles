76ec00f1483ce40dffeba0517dc19330b084ea36
Yoshiyuki Abe <abe-yoshiyuki@kit.co.jp>
2016-05-16 01:34:35 +0000

    .

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -5,7 +5,7 @@ ln -sf ~/dotfiles/colors ~/.vim/.
 ln -sf ~/dotfiles/ftdetect ~/.vim/.
 ln -sf ~/dotfiles/indent ~/.vim/.
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
+#ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
 #ln -sf ~/dotfiles/Brewfile ~/Brewfile
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 echo "run: curl -L git.io/nodebrew | perl - setup"
